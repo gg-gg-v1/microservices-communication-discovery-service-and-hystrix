48db647ddd094cfe5f47c234e23f8eab33b3d1fc
Bharat2020suthar1600597573882 <bharat.svnit99@gmail.com>
2022-02-20 21:04:16 +0530

    adding spring cloud api gateway

---
+++ api-gateway-based-on-spring-cloud/src/test/java/io/sample/apigateway/apigatewaybasedonspringcloud/ApiGatewayBasedOnSpringCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sample.apigateway.apigatewaybasedonspringcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayBasedOnSpringCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
